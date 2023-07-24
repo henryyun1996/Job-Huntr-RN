0f09e57916c8562ed5230e09ee44b13a61a51827
Christian <hernandezchristian94@gmail.com>
2023-07-24 03:15:42 -0700

    updated

--- client/components/UserInfo/UserProfile/UserProfile.js
+++ client/components/UserInfo/UserProfile/UserProfile.js
@@ -2,7 +2,6 @@ import { Text, View, Image, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
 function UserProfile({ user, setUser }) {
-    console.log(user)
 
     const handleLogout = () => {
         fetch('https://5341-2603-8001-4800-2320-3ca7-c4c7-d437-61fb.ngrok-free.app/logout', {
@@ -22,8 +21,8 @@ function UserProfile({ user, setUser }) {
                     style={{ width: 80, height: 80 }}
                 />
                 <View style={styles.userInfoWrapper}>
-                    <Text style={styles.fname}>{user.fname}</Text>
-                    <Text style={styles.email}>{user.email}</Text>
+                    <Text style={styles.fname}>{user.user.fname} {user.user.lname}</Text>
+                    <Text style={styles.email}>{user.user.email}</Text>
                 </View>
             </View>
             <View style={styles.userNav}>
